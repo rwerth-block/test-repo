49736fe47fe0240331a7acb4eb81605769dd86eb
rwerth-block <rwerth-block>
2025-10-07 20:01:48 +0000

    no key

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,4 @@
 if __name__ == "__main__":
     print("Hello, World!")
 
-    api_key = "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"
+    api_key = ""
